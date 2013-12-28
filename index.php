39f70aafd8664e0045a52cb13defdacb874f4724
Manuel Fernandes <manuelferns_007@yahoo.co.in>
2013-12-28 17:24:47 +0530

    PHP template trial for static page
    Demo

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$title = "Travel-Template 1";
+$content = '<img src="img/template-1/singapore.jpeg" class="imgLeft" />
+<h3>Singapore</h3>
+<p>
+   Singapore  officially the Republic of Singapore, is a sovereign city-state and island country in Southeast Asia. It lies off the southern tip of the Malay Peninsula and is 137 kilometres (85 mi) north of the equator. Made up of the lozenge-shaped main island (widely known as Singapore Island but also as Pulau Ujong, its native Malay name) and over 60 much smaller islets,[8] it is separated from Peninsular Malaysia by the Straits of Johor to its north and from Indonesias Riau Islands by the Singapore Strait to its south. The country is highly urbanised, with very little primary rainforest remaining. Its territory has consistently expanded through land reclamation.
+Part of various local empires since being settled in the second century AD, modern Singapore was founded in 1819 by Sir Stamford Raffles as a trading post of the East India Company with the permission of the Johor Sultanate. The British obtained sovereignty over the island in 1824, and Singapore became one of the British Straits Settlements in 1826. Occupied by the Japanese during World War II, Singapore declared independence from the United Kingdom in 1963 and united with other former British territories to form Malaysia, from which it departed two years later. Since then, it has developed rapidly, earning recognition as one of Four Asian Tigers.
+</p>
+
+<img src="img/template-1/usa.jpeg" class="imgRight"/>
+<h3>USA</h3>
+<p>
+    Since 1998 USA Tourism have been helping tourism partners market their properties online.
+    Now weve expanded our website to allow our partners to display the information that matters most
+    to the traveling public. Amenities, photos, Google Maps, web links, rating, customer reviews and much more.
+Were more than a website directory. Wre an online travel resource, serving people with a passion to travel and
+we want to introduce them to your property.
+</p>
+
+';
+
+include 'template-1.php';
+?>
+
